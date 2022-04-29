04f283fe995b66a62b1bc0244faa9100f4a5e7c0
Teafedos <tefedos99@gmail.com>
2022-04-29 13:53:12 +0300

    correct errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_04_28_103217) do
+  create_table "clients", force: :cascade do |t|
+    t.string "login"
+    t.string "password_digest"
+    t.string "mail"
+    t.string "nickname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "login"
     t.string "password"
